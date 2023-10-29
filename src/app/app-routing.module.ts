5bab0d277adc0cf3f61ba4f81f379455839b556c
Umberto <paolassoumberto@gmail.com>
2023-10-29 20:40:35 +0100

    ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,10 @@ import { dettaglioComponent } from './dettaglio/dettaglio.component';
 const routes: Routes = [
   { path: 'home', component: homeComponent },
   { path: 'dettaglio/:id', component: dettaglioComponent },
-  // { path: "details", component: homeComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' }, // redirect to `first-component`
   { path: '**', component: PageNotFound }, // route for a 404 page
-]; // sets up routes constant where you define your routes
+];
 
-// configures NgModule imports and exports
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
--- src/app/dettaglio/dettaglio.component.ts
+++ src/app/dettaglio/dettaglio.component.ts
@@ -1,17 +1,17 @@
-import { Component } from "@angular/core";
-import { ApiService } from "../ApiService";
-import { ActivatedRoute } from "@angular/router";
+import { Component } from '@angular/core';
+import { ApiService } from '../ApiService';
+import { ActivatedRoute } from '@angular/router';
 
 type DrinkResponse = { strIngredient: string; strMeasure1: string };
 
 @Component({
-  selector: "dettaglioComponent",
-  templateUrl: "./dettaglio.component.html",
-  styleUrls: ["./dettaglio.component.css"],
+  selector: 'dettaglioComponent',
+  templateUrl: './dettaglio.component.html',
+  styleUrls: ['./dettaglio.component.css'],
 })
 export class dettaglioComponent {
   id: any;
-  drink: any; // Specifica il tipo corretto
+  drink: any;
 
   newDrink: DrinkResponse[] = [];
 
@@ -20,12 +20,12 @@ export class dettaglioComponent {
   ngOnInit() {
     // Recupera l'ID dalla URL
     this.route.paramMap.subscribe((params) => {
-      this.id = params.get("id");
+      this.id = params.get('id');
     });
 
     this.apiService.getDrinkDetails(this.id).subscribe((data) => {
       this.drink = data;
-      console.log("->", this.drink);
+      console.log('->', this.drink);
 
       this.newDrink = Array.from({ length: 15 }, (_, i) => ({
         strIngredient: this.drink.drinks[0][`strIngredient${i + 1}`],
